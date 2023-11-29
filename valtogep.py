6c47a3280206e730c8a290407780fd55e2044164
hatiferenc1 <72908866+hatiferenc1@users.noreply.github.com>
2023-11-29 16:07:22 +0100

    Update valtogep.py

--- valtogep.py
+++ valtogep.py
@@ -7,11 +7,10 @@ st.text(format(2**level))
 
 
 
-
 data = {
-   'x' : [i**2 for i in range(hatvanykitevo)]}
+   'x' : [i**2 for i in range(level)]}
 
-dataframe = pd.DataFrame(level)
+dataframe = pd.DataFrame(data)
 st.write('This is a line_chart.')
 st.line_chart(dataframe)
 
