f125ef0ffd6ad56f147cd6451a91266d67ba3eb2
hatiferenc1 <72908866+hatiferenc1@users.noreply.github.com>
2023-11-29 16:06:27 +0100

    Update valtogep.py

--- valtogep.py
+++ valtogep.py
@@ -6,10 +6,12 @@ level = st.slider("válassz egy számot", 1, 16)
 st.text(format(2**level))
 
 
+
+
 data = {
    'x' : [i**2 for i in range(hatvanykitevo)]}
 
-dataframe = pd.DataFrame(data)
+dataframe = pd.DataFrame(level)
 st.write('This is a line_chart.')
 st.line_chart(dataframe)
 
