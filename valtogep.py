f0b4ad8b028c1f517ebddec64932d107a7c19629
hatiferenc1 <72908866+hatiferenc1@users.noreply.github.com>
2023-11-29 16:03:56 +0100

    Update valtogep.py

--- valtogep.py
+++ valtogep.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import pandas as pd 
+import numpy as np
 
 level = st.slider("válassz egy számot", 1, 16)
 st.text(format(2**level))
 
-import pandas as pd 
+
+ hatvany = {
+   'x' : [i**2 for i in range(hatvanykitevo)]}
+
+dataframe = pd.DataFrame(data)
+st.write('This is a line_chart.')
+st.line_chart(dataframe)
 
 
 
