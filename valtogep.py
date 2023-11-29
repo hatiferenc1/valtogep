bfeb78a5414c0c111ede6469fa7eca5375138468
hatiferenc1 <72908866+hatiferenc1@users.noreply.github.com>
2023-11-29 15:28:42 +0100

    Update valtogep.py

--- valtogep.py
+++ valtogep.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
+level = st.slider(".slider: Select the level", 1, 16)
+st.text(format(2**level))
 
-level = st.slider(".slider: Select the level", 1, 5)
-
-st.text('Selected: {}'.format(level))
